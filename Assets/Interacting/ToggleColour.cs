c0b82dbc919aa6aa29f802c742ff9301e29939b2
James Bacon <james@baconi.co.uk>
2020-07-19 12:19:05 +0100

    Interactable circles.

---
+++ Assets/Interacting/ToggleColour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace Interacting
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class ToggleColour : Interactable
+    {
+        [SerializeField] private Color first = Color.red;
+        [SerializeField] private Color second = Color.yellow;
+        
+        private SpriteRenderer _spriteRenderer;
+
+        protected void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+            _spriteRenderer.color = first;
+        }
+
+        protected override void OnInteraction()
+        {
+            // Toggle colour
+            _spriteRenderer.color = _spriteRenderer.color == first ? second : first;
+        }
+    }
+}
