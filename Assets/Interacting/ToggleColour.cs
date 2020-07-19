440ee753c5b5c9bb1e8f7f2e5ae2c043eb637ec3
James Bacon <james@baconi.co.uk>
2020-07-19 13:19:56 +0100

    Pre-removal of aaka

--- Assets/Interacting/ToggleColour.cs
+++ Assets/Interacting/ToggleColour.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Akka.Actor;
+using Logic;
+using TMPro;
 using UnityEngine;
 
 namespace Interacting
@@ -8,19 +10,31 @@ namespace Interacting
     {
         [SerializeField] private Color first = Color.red;
         [SerializeField] private Color second = Color.yellow;
-        
+        [SerializeField] private AkkaSystem akkaSystem;
+        [SerializeField] private TextMeshProUGUI textField;
+
+        private IActorRef _textFieldActor;
         private SpriteRenderer _spriteRenderer;
 
         protected void Awake()
         {
             _spriteRenderer = GetComponent<SpriteRenderer>();
             _spriteRenderer.color = first;
+
+            // Setup simple actor
+            _textFieldActor = akkaSystem.CreateActor(ColourToTextUntypedActor.Props(textField));
         }
 
         protected override void OnInteraction()
         {
             // Toggle colour
-            _spriteRenderer.color = _spriteRenderer.color == first ? second : first;
+            Color colourToDisplay = _spriteRenderer.color == first ? second : first;
+            
+            // Updated display
+            _spriteRenderer.color = colourToDisplay;
+            
+            // Tell text field actor about the change
+            _textFieldActor.Tell(new ColourToTextUntypedActor.ColourChanged(colourToDisplay));
         }
     }
 }
---
+++ Assets/Logic/ColourToTextUntypedActor.cs
@@ -0,0 +1,42 @@
+﻿using Akka.Actor;
+using TMPro;
+using UnityEngine;
+
+namespace Logic
+{
+    public class ColourToTextUntypedActor : UntypedActor
+    {
+        private readonly TextMeshProUGUI _textField;
+
+        private ColourToTextUntypedActor(TextMeshProUGUI textField)
+        {
+            _textField = textField;
+        }
+        
+        public class ColourChanged
+        {
+            public readonly Color Colour;
+
+            public ColourChanged(Color colour)
+            {
+                Colour = colour;
+            }
+        }
+        
+        protected override void OnReceive(object message)
+        {
+            switch (message)
+            {
+                case ColourChanged changed:
+                    _textField.text = changed.Colour.ToString();
+                    _textField.color = changed.Colour;
+                    break;
+            }
+        }
+        
+        public static Props Props(TextMeshProUGUI textField)
+        {
+            return Akka.Actor.Props.Create(() => new ColourToTextUntypedActor(textField));
+        }
+    }
+}
