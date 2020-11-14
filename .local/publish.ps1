cb76f549b52fdfbb215a8c355a795c4098e57f58
James Bacon <james@baconi.co.uk>
2020-11-14 12:39:20 +0000

    Fixed silly mistake.

--- .local/publish.ps1
+++ .local/publish.ps1
@@ -8,6 +8,6 @@
 ##
 ItchIoPublish -BuildTarget StandaloneWindows64 -Project beercan/playground-unity-ci -Channel windows
 ItchIoPublish -BuildTarget StandaloneLinux64 -Project beercan/playground-unity-ci -Channel linux
-ItchIoPublish -BuildTarget StandaloneOSX -Project beercan/playground-unity-cie -Channel osx
+ItchIoPublish -BuildTarget StandaloneOSX -Project beercan/playground-unity-ci -Channel osx
 ItchIoPublish -BuildTarget Android -Project beercan/playground-unity-ci -Channel android
 ItchIoPublish -BuildTarget WebGL -Project beercan/playground-unity-ci -Channel webgl
